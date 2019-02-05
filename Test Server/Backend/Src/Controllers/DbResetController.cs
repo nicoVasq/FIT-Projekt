d53f508d210e664fd5126b3ad81c397558703421
nico <nico.vasquez@gmx.at>
2019-02-05 12:31:02 +0100

    Add console log for deleted items #18FIT-23 To Verify

--- Test Server/Backend/Src/Controllers/DbResetController.cs
+++ Test Server/Backend/Src/Controllers/DbResetController.cs
@@ -10,6 +10,8 @@ using System.Net.Http;
 using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
+using System.Diagnostics;
+
 namespace Backend.Src.Controllers
 {
     [Route("api/resetdb")]
@@ -26,40 +28,58 @@ namespace Backend.Src.Controllers
         [HttpGet]
         public void DeleteDbRequest()
         {
+            Console.WriteLine("\nDeleting\n#------------------------------#");
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
             Booking booking = _uow.BookingRepository.Get().OrderBy(b => b.Id).Last();
             Company company = _uow.CompanyRepository.Get().OrderBy(c => c.Id).Last();
 
             var bookingBranches = _uow.BookingBranchesRepository.Get().Where(br => br.fk_Booking == booking.Id);
+            Console.WriteLine($"BookingBranches: {bookingBranches.Count()}");
             foreach (var item in bookingBranches)
             {
+                Console.WriteLine($"Id:{item.Id} BookingId:{item.Booking.Id} Branch:{item.Branch.Name}");
                 _uow.BookingBranchesRepository.Delete(item);
             }
 
-            var companyBranch = _uow.CompanyBranchRepository.Get().Where(cb => cb.fk_Company == company.Id);
-            foreach (var item in companyBranch)
+            var companyBranches = _uow.CompanyBranchRepository.Get().Where(cb => cb.fk_Company == company.Id);
+            Console.WriteLine($"\nCompanyBranches: {companyBranches.Count()}");
+            foreach (var item in companyBranches)
             {
+                Console.WriteLine($"Id:{item.Id} Company:{item.Comapny.Name} Branch:{item.Branch.Name}");
                 _uow.CompanyBranchRepository.Delete(item);
             }
 
             var resourceBooking = _uow.ResourceBookingRepository.Get().Where(rb => rb.fk_Booking == booking.Id);
+            Console.WriteLine($"\nResourceBooking: {resourceBooking.Count()}");
             foreach (var item in resourceBooking)
             {
+                Console.WriteLine($"Id:{item.Id} BookingId:{item.Booking.Id} Resource:{item.Resource.Name}");
                 _uow.ResourceBookingRepository.Delete(item);
             }
 
+            Console.WriteLine($"\nRepresentatives: {booking.Representatives.Count()}");
             foreach (var item in booking.Representatives)
             {
+                Console.WriteLine($"Id:{item.Id} Name:{item.Name}");
                 _uow.RepresentativeRepository.Delete(item);
             }
 
-            _uow.AddressRepository.Delete(company.fk_Address);  
+            Console.WriteLine($"\nAdress\nId:{company.Address.Id} - {company.Address.Street} {company.Address.ZipCode}\n");
+            _uow.AddressRepository.Delete(company.fk_Address);
 
+            Console.WriteLine($"Booking\nId:{booking.Id} Company:{booking.Company.Name}");
             _uow.BookingRepository.Delete(booking.Id);
+            Console.WriteLine($"Company\nId:{company.Id} Name:{company.Name}");
             _uow.CompanyRepository.Delete(company.Id);
 
+
             var contacts = _uow.ContactRepository.Get().Where(c => c.Id == company.fk_Contact || c.Id == booking.fk_Contact);
+            Console.WriteLine($"\nContacts: {contacts.Count()}");
             foreach (var item in contacts)
             {
+                Console.WriteLine($"Id:{item.Id} Name:{item.FirstName} {item.LastName}");
                 _uow.ContactRepository.Delete(item);
             }
 
@@ -70,6 +90,23 @@ namespace Backend.Src.Controllers
 
             _uow.Save();
 
+
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}.{2:00}", ts.Minutes, ts.Seconds,
+               ts.Milliseconds / 10);
+            Console.WriteLine("...\nFinished\nTime elapsed: " + elapsedTime);
+
+        }
+
+        [HttpGet("{name}")]
+        [ProducesResponseType(typeof(Company), StatusCodes.Status200OK)]
+        public IActionResult Delete(string name)
+        {
+            Company company = _uow.CompanyRepository.Get().SingleOrDefault(c => c.Name == name);
+            Booking booking = _uow.BookingRepository.Get().SingleOrDefault(b => b.fk_Company == company.Id);
+
+            return new ObjectResult(company);
         }
     }
 }
